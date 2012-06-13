10c386902440257df2dc654cbf75e99e506aa8ca
Mary Cutrali <mary.cutrali@gmail.com>
2012-06-13 12:47:32 -0400

    parse recipient field for chatroom id

--- app/models/incoming_email.rb
+++ app/models/incoming_email.rb
@@ -1,14 +1,20 @@
 class IncomingEmail < ActiveRecord::Base
   attr_accessible :body, :subject, :sender, :recipient
+  #:chatroom_id
 
   def self.receive_email(message)
     subject = message.subject 
     unless !subject.present? 
       ie = IncomingEmail.new(subject:message.subject,
-                        recipient:message.to.decoded, 
+                        recipient:message.to, 
                         body:message.body.decoded, 
                         sender:message.from.first)
+                        #chatroom_id:get_chatroom_id(message.to))
       ie.save
     end
   end
+
+  # def get_chatroom_id(message)
+  #   chatroom_id = message.match(/[\d{1,10}]/).to_s
+  # end 
 end
