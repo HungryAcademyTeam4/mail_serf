112d182d186b5e3f067d31047558177244f27830
Mary Cutrali <mary.cutrali@gmail.com>
2012-06-13 01:27:31 -0400

    first commits

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailSerf::Application.config.secret_token = 'e30b24caee9701d249a008961b6b8af9261d2fc7dd11194e30fbad4a9f9f8aa50d3ce213411039a4c9835e0b30d1d76a72dd78288893f15248243443583a1cd1'
