05c13a02cf147a4b83aa311423ff2ea2925043f6
Mary Cutrali <mary.cutrali@gmail.com>
2012-06-13 15:11:34 -0400

    adding enviro variables for email address

---
+++ config/initializers/constants.rb
@@ -0,0 +1,7 @@
+if Rails.env == "production"
+  MAILMAN_USER     = "conquerchat@gmail.com"
+  MAILMAN_PASSWORD = "chatpassword"
+else
+  MAILMAN_USER     = "fallingpostfix@gmail.com"
+  MAILMAN_PASSWORD = "13myra13"
+end
